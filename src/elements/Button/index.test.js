fe0c952dbd683ec22f286550135e35ff9027b1d2
mkharismaula <63088315+mkharismaula@users.noreply.github.com>
2020-05-05 13:57:30 +0700

    Initial commit

---
+++ src/elements/Button/index.test.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Button from './index'
+
+test("Should not allow click button if isDisable is present", () => {
+    const { container } = render(<Button isDisable></Button>);
+
+    expect(container.querySelector("span.disable")).toBeInTheDocument();
+});
+
+test("Should render loading/spinner", () => {
+    const { container,getByText } = render(<Button isLoading></Button>);
+
+    expect(getByText(/loading/i)).toBeInTheDocument();
+    expect(container.querySelector("span")).toBeInTheDocument();
+});
+
+test("Should render <a> tag", () => {
+    const { container } = render(<Button type="Link" isExternal></Button>);
+
+    expect(container.querySelector("a")).toBeInTheDocument();
+});
+
+test("Should render <Link> component", () => {
+    const { container } = render(<Router><Button href=""  type="Link"></Button></Router>);
+
+    expect(container.querySelector("a")).toBeInTheDocument();
+});
