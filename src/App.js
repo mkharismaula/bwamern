24c515376b8f904f1b9837795c31499769d49ad6
mkharismaula <63088315+mkharismaula@users.noreply.github.com>
2020-05-11 19:24:24 +0700

    new

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import 'assets/scss/style.scss';
 import LandingPage from 'pages/LandingPage';
+import DetailsPage from 'pages/DetailsPage';
+import Example from 'pages/Example';
+import Checkout from 'pages/Checkout'
+
+import 'assets/scss/style.scss';
 
 function App() {
   return <div className="App">
       <Router>
-        <Route path="/" component={LandingPage}></Route>
+        <Route exact path="/" component={LandingPage}></Route>
+        <Route exact path="/properties/:id" component={DetailsPage}></Route>
+        <Route exact path="/checkout" component={Checkout}></Route>
+        <Route path="/example" component={Example}></Route>
       </Router>
     </div>;
 }
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,7 +6,7 @@ import Hero from 'parts/Hero';
 import MostPicked from 'parts/MostPicked';
 import Categories from 'parts/Categories';
 import Testimony from 'parts/Testimony';
-import Footer from 'parts/Footer'
+import Footer from 'parts/Footer';
 
 export default class LandingPage extends Component{
     constructor(props) {
